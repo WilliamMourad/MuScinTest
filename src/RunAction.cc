afa8a1f48b6240a30368303e0f552bc9cea19bd4
generalemike <58291160+generalemike@users.noreply.github.com>
2025-10-30 21:45:57 +0100

    Resolved warnings related to energy cut printing
    This change resolves some warnings that occurred when printing the energy cut from the RunAction::RunAction constructor.
    It was solved relocating the print logic to RunAction::BeginOfRunAction.

--- src/RunAction.cc
+++ src/RunAction.cc
@@ -10,22 +10,6 @@ RunAction::RunAction(RunActionParameters runActionParameters)
 
 	timer = new G4Timer();
 
-	// Print the energy cuts corresponding to the range cuts.
-	// I need these values to evaluate the restricted Landau/Vavilov edep distribution.
-	// I should put this in a custom Geant4 command that can be called on demand
-	// but to keep things simple, for now I will just leave it here (expect some repeated output in MT mode).
-	if (_runActionParameters.enableCuts)
-	{
-		G4EmCalculator calc;
-		auto Tcut_scint = calc.ComputeEnergyCutFromRangeCut(30 * um, "e-", "G4_PLASTIC_SC_VINYLTOLUENE");
-		auto Tcut_si = calc.ComputeEnergyCutFromRangeCut(10 * um, "e-", "G4_Si");
-		auto Tcut_al = calc.ComputeEnergyCutFromRangeCut(2 * um, "e-", "G4_Al");
-
-		G4cout << "Tcut (MeV): scint=" << Tcut_scint / MeV
-			<< " si=" << Tcut_si / MeV
-			<< " al=" << Tcut_al / MeV << G4endl;
-	}
-
 	analysisManager = G4AnalysisManager::Instance();
 	analysisManager->Reset();
 	analysisManager->SetVerboseLevel(1);
@@ -63,6 +47,23 @@ RunAction::~RunAction()
 
 void RunAction::BeginOfRunAction(const G4Run* run)
 {
+
+	// Print the energy cuts corresponding to the range cuts.
+	// I need these values to evaluate the restricted Landau/Vavilov edep distribution.
+	// I should put this in a custom Geant4 command that can be called on demand
+	// but to keep things simple, for now I will just leave it here (expect some repeated output in MT mode).
+	if (_runActionParameters.enableCuts)
+	{
+		G4EmCalculator calc;
+		auto Tcut_scint = calc.ComputeEnergyCutFromRangeCut(30 * um, "e-", "G4_PLASTIC_SC_VINYLTOLUENE");
+		auto Tcut_si = calc.ComputeEnergyCutFromRangeCut(10 * um, "e-", "G4_Si");
+		auto Tcut_al = calc.ComputeEnergyCutFromRangeCut(2 * um, "e-", "G4_Al");
+
+		G4cout << "Tcut (MeV): scint=" << Tcut_scint / MeV
+			<< " si=" << Tcut_si / MeV
+			<< " al=" << Tcut_al / MeV << G4endl;
+	}
+
 	timer->Start();
 
 	std::string outputDir = _runActionParameters.outputDir;
